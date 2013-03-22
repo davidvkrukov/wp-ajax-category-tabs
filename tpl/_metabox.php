abfc1afaba972851866b826d0982414ee2d3c928
David V. Krukov <david.v.krukov@gmail.com>
2013-03-22 23:15:09 +0200

    CSS

--- tpl/_metabox.php
+++ tpl/_metabox.php
@@ -1,27 +1,33 @@
 <?php global $post; ?>
 <?php wp_nonce_field(plugin_basename(__FILE__),'wpact_nonce'); ?>
-<h4>Select flag(s):</h4>
+<h2>Select flag(s)</h2>
 <ul>
+<?php 
+$_flags=unserialize(get_post_meta($post->ID,'wpact_flag',true));
+if(!is_array($_flags)){
+	$_flags=array();
+}
+?>
 <?php $i=1;foreach($flags as $_f=>$_url): ?>
-	<li style="float:left;background:#eee;margin:2px;">
+	<li style="float:left;background:<?php echo (in_array($_f,$_flags)?'#aae':'#eee') ?>;margin:2px;">
 		<label for="flag_<?php echo $_f?>">
 			<strong><?php echo strtoupper($_f); ?></strong>
 			<img src="<?php echo $_url ?>"/>
 		</label><br/>
 		<div style="width:100%;text-align:center;">
-			<input type="checkbox" id="flag_<?php echo $_f?>" name="wpact_flag[]" value="<?php echo $_f ?>"/>
+			<input type="checkbox" id="flag_<?php echo $_f?>"<?php echo (in_array($_f,$_flags)?' checked="1"':''); ?> name="wpact_flag[]" value="<?php echo $_f ?>"/>
 		</div>
 	</li>
 <?php $i++;endforeach; ?>
 </ul>
 <div style="clear:both;"></div>
 <?php $source=get_post_meta($post->ID,'wpact_source',true); ?>
-<h4>Source link:</h4>
-<label for="wpact_source_url">
-	<strong>URL</strong>
-	<input type="text" id="wpact_source_url" name="wpact_source[url]" value="<?php echo isset($source['url'])?$source['url']:''; ?>"/>
-</label>
+<h2>Source link</h2>
 <label for="wpact_source_title">
-	<strong>Title</strong>
-	<input type="text" id="wpact_source_title" name="wpact_source[title]" value="<?php echo isset($source['title'])?$source['title']:''; ?>"/>
-</label>
+	<strong style="display:block;">Title: </strong>
+	<input style="width:45%;" type="text" id="wpact_source_title" name="wpact_source[title]" value="<?php echo isset($source['title'])?$source['title']:''; ?>"/>
+</label><br/>
+<label for="wpact_source_url" style="width:25%;">
+	<strong style="display:block;">Link URL: </strong>
+	<input style="width:45%;" type="text" id="wpact_source_url" name="wpact_source[url]" value="<?php echo isset($source['url'])?$source['url']:''; ?>"/>
+</label><br/>
